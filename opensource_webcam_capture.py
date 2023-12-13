b33acc8afbfa87276f45207b86d3ef6746f32baa
Ahn Soo Bin <ans525857@gmail.com>
2023-12-14 04:11:04 +0900

    Add Face Detection

--- opensource_webcam_capture.py
+++ opensource_webcam_capture.py
@@ -20,6 +20,24 @@ while time.time() - start_time < 5:
     if cv2.waitKey(1) == ord('q'):
         break
 
-# Release the webcam and close OpenCV windows
-cap.release()
+# Load the pre-trained Haar Cascade classifier for frontal faces
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Read the image from the webcam capture
+img = cv2.imread('Webcam Capture.jpg')
+
+# Detect faces in the image using the detectMultiScale method
+# Parameters: scaleFactor, minNeighbors, minSize
+faces = face_cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+# Iterate over detected faces and print their positions
+for (x, y, w, h) in faces:
+    print(f"Detected face position: x={x}, y={y}, width={w}, height={h}")
+
+# Display the original image with rectangles around detected faces
+cv2.imshow('detected face', img)
+
+# Wait for a key press and then close the image window
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+cap.release()
